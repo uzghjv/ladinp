e9fdd0bd41cc6495c2a54d8fdee937a995a7b81c
hinapeng <3390127536@qq.com>
2026-09-19 01:54:43 +0800

    second commit

--- main.c
+++ main.c
@@ -4,5 +4,5 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("Hello, world!\n");
-    printf("God does not play dice with the universe\n")
+    printf("God does not play dice with the universe\n");
 }
